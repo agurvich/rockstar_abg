fd92b94cfef84adc25d35d7dd554e23d3d5c9ad4
Alex Gurvich <agurvich@u.northwestern.edu>
2022-01-19 20:00:52 -0600

    manually track modifications to andrews code

---
+++ modifications.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+zip modifications.zip \
+src/rockstar_abg/gizmo_analysis/gizmo_agetracer.py \
+src/rockstar_abg/gizmo_analysis/gizmo_diagnostic.py \
+src/rockstar_abg/gizmo_analysis/gizmo_file.py \
+src/rockstar_abg/gizmo_analysis/gizmo_group.py \
+src/rockstar_abg/gizmo_analysis/gizmo_ic.py \
+src/rockstar_abg/gizmo_analysis/gizmo_io.py \
+src/rockstar_abg/gizmo_analysis/gizmo_plot.py \
+src/rockstar_abg/gizmo_analysis/gizmo_star.py \
+src/rockstar_abg/gizmo_analysis/gizmo_track.py \
+src/rockstar_abg/utilities/io.py
