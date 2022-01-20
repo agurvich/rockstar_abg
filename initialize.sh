83c59f5c07d2f09ef693364f16e9cd33a6df122a
Alex Gurvich <agurvich@u.northwestern.edu>
2022-01-19 19:37:58 -0600

    initial commit

---
+++ initialize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git submodule add ./reference/halo_submit reference/halo_submit
+git submodule add ./src/rockstar_abg/gizmo_analysis src/rockstar_abg/gizmo_analysis
+git submodule add ./src/rockstar_abg/halo_analysis src/rockstar_abg/halo_analysis
+git submodule add ./src/rockstar_abg/utilities src/rockstar_abg/utilities
+git submodule add ./src/rockstar_abg/executables/rockstar-galaxies src/rockstar_abg/executables/rockstar-galaxies
+git submodule add ./src/rockstar_abg/executables/consistent-trees src/rockstar_abg/executables/consistent-trees
---
+++ runner.py
@@ -0,0 +1,7 @@
+from rockstar_abg.handler import main as handler
+
+def main():
+    handler('m12i_m6e4','fire3_compatability/core_no_bh',)
+
+if __name__ == '__main__':
+    main()
