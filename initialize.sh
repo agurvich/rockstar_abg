de5875f0c07937fd8c970e640021b431d964b418
Alex Gurvich <agurvich@u.northwestern.edu>
2022-06-27 14:17:20 -0500

    initialize submodules with correct urls

--- initialize.sh
+++ initialize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git submodule add ./reference/halo_submit reference/halo_submit
-git submodule add ./src/rockstar_abg/gizmo_analysis src/rockstar_abg/gizmo_analysis
-git submodule add ./src/rockstar_abg/halo_analysis src/rockstar_abg/halo_analysis
-git submodule add ./src/rockstar_abg/utilities src/rockstar_abg/utilities
-git submodule add ./src/rockstar_abg/executables/rockstar-galaxies src/rockstar_abg/executables/rockstar-galaxies
-git submodule add ./src/rockstar_abg/executables/consistent-trees src/rockstar_abg/executables/consistent-trees
+git submodule add --force git@bitbucket.org:awetzel/halo_submit.git reference/halo_submit
+git submodule add --force git@bitbucket.org:awetzel/consistent-trees.git src/rockstar_abg/executables/consistent-trees
+git submodule add --force git@bitbucket.org:awetzel/rockstar-galaxies.git src/rockstar_abg/executables/rockstar-galaxies
+git submodule add --force git@bitbucket.org:awetzel/gizmo_analysis.git src/rockstar_abg/gizmo_analysis
+git submodule add --force git@bitbucket.org:awetzel/halo_analysis.git src/rockstar_abg/halo_analysis
+git submodule add --force git@bitbucket.org:awetzel/utilities.git src/rockstar_abg/utilities
