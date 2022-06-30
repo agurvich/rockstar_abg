dca2ce31ef1ed79c757368bc40efbed9384e6a3f
Alex Gurvich <agurvich@u.northwestern.edu>
2022-06-30 16:59:42 -0500

    track my repos as submodules

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git submodule add --force git@bitbucket.org:awetzel/rockstar-galaxies.git src/rockstar_abg/executables/rockstar-galaxies-fire2
-git submodule add --force git@bitbucket.org:awetzel/halo_submit.git reference/halo_submit
-git submodule add --force git@bitbucket.org:awetzel/consistent-trees.git src/rockstar_abg/executables/consistent-trees
-git submodule add --force git@bitbucket.org:awetzel/rockstar-galaxies.git src/rockstar_abg/executables/rockstar-galaxies
-git submodule add --force git@bitbucket.org:awetzel/gizmo_analysis.git src/rockstar_abg/gizmo_analysis
-git submodule add --force git@bitbucket.org:awetzel/halo_analysis.git src/rockstar_abg/halo_analysis
-git submodule add --force git@bitbucket.org:awetzel/utilities.git src/rockstar_abg/utilities
+git submodule add --force git@bitbucket.org:agurvich/rockstar-galaxies.git src/rockstar_abg/executables/rockstar-galaxies-fire2
+git submodule add --force git@bitbucket.org:agurvich/halo_submit.git reference/halo_submit
+git submodule add --force git@bitbucket.org:agurvich/consistent-trees.git src/rockstar_abg/executables/consistent-trees
+git submodule add --force git@bitbucket.org:agurvich/rockstar-galaxies.git src/rockstar_abg/executables/rockstar-galaxies
+git submodule add --force git@bitbucket.org:agurvich/gizmo_analysis.git src/rockstar_abg/gizmo_analysis
+git submodule add --force git@bitbucket.org:agurvich/halo_analysis.git src/rockstar_abg/halo_analysis
+git submodule add --force git@bitbucket.org:agurvich/utilities.git src/rockstar_abg/utilities
