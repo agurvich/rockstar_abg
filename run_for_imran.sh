3fd995e73e6b5658accf07292d8c402004bd548b
Alex Gurvich <agurvich@u.northwestern.edu>
2022-07-18 13:46:38 -0500

    turn run_for_imran.sh into job_maker.sh (will rename in next commit)

--- run_for_imran.sh
+++ run_for_imran.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-sim_path=/projects/b1026/isultan/halos
+## usually one of bash, less, sbatch, what to execute on the dummy script
+this_command=$1 
+
+## job preamble with allocation, etc...
+preamble=${HOME}/ss/preamble.sh
+
+## target directory whose simulations we should loop over and generate halo files for
+sim_path=/projects/b1026/snapshots/restartdisktest/m11a_m2e3
 
 for sim_dir in ${sim_path}/*
 do
     name=`basename ${sim_dir}`
     ## arg0: directory to simulations
     ## arg1: name of simulation
-    python runner.py ${sim_path} ${name}
+    if [ -d  ${sim_dir}/output ]
+    then
+
+        ## sets job_name to the last string delimited by _ (lol)
+        IFS='_' read -ra ADDR <<< ${name}
+        for job_name in "${ADDR[@]}"; do
+            sleep 0
+        done
+
+        cat ${preamble} > temp.sh 
+        # don't need this because it's already in my preamble on Quest
+        #echo "#SBATCH -t 48:00:00        # run time (hh:mm:ss) - 48 hours" >> temp.sh
+        echo "#SBATCH -J ${job_name}_rockstar         # job name" >> temp.sh
+        echo "python runner.py ${sim_path} ${name}" >> temp.sh
+        ${this_command} temp.sh
+        rm temp.sh
+    else
+        echo ${name} "doesn't have an output directory"
+        sleep 1
+    fi
+
 done
