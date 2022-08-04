ec478baad36f41e122abf260166332789402abcb
Alex Gurvich <agurvich@u.northwestern.edu>
2022-08-04 08:41:40 -0500

    add skip counter and module load hdf5 to job_maker

--- job_maker.sh
+++ job_maker.sh
@@ -7,31 +7,36 @@ this_command=$1
 preamble=${HOME}/ss/preamble.sh
 
 ## target directory whose simulations we should loop over and generate halo files for
-sim_path=/projects/b1026/snapshots/MassiveFIRE
+sim_path=${HOME}/snaps/fire3_compatability/core
+
+## how many should we skip at the beginning (i.e. b.c. we are running interactively)
+skip=1
 
 for sim_dir in ${sim_path}/*
 do
     name=`basename ${sim_dir}`
     ## arg0: directory to simulations
     ## arg1: name of simulation
-    if [ -d  ${sim_dir}/output ]
+    if [[ -d  ${sim_dir}/output  && ${skip} -le 0 ]]
     then
 
-        if [ -f ${sim_dir}/halo/rockstar_dm/catalog_hdf5/tree.hdf5 ] 
+        if [ -f ${sim_dir}/halo/rockstar_dm/catalog_hdf5/tree.hdf5 ]
         then
             echo ${name} "is already done!"
             sleep 1
         else
             ## sets job_name to the last string delimited by _ (lol)
-            IFS='_' read -ra ADDR <<< ${name}
-            for job_name in "${ADDR[@]}"; do
-                sleep 0
-            done
+            #IFS='_' read -ra ADDR <<< ${name}
+            #for job_name in "${ADDR[@]}"; do
+                #sleep 0
+            #done
+            job_name=${name}
 
             cat ${preamble} > temp.sh 
             # don't need this because it's already in my preamble on Quest
             #echo "#SBATCH -t 48:00:00        # run time (hh:mm:ss) - 48 hours" >> temp.sh
             echo "#SBATCH -J ${job_name}_rockstar         # job name" >> temp.sh
+            echo "module load hdf5" >> temp.sh
             echo "python runner.py ${sim_path} ${name}" >> temp.sh
             echo ${name} "missing tree.hdf5"
             ${this_command} temp.sh
@@ -41,5 +46,8 @@ do
         echo ${name} "has no snaps"
         sleep 1
     fi
+
+    ## decrement the skip counter
+    skip=$((skip-1))
 
 done
